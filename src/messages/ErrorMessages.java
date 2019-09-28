ea28412cf291fb43ece5123d0cb54c2b38dfa9cc
zspace <zstefchev@gmail.com>
2019-09-28 21:24:08 +0300

    .

--- src/messages/ErrorMessages.java
+++ src/messages/ErrorMessages.java
@@ -1,4 +1,13 @@
 package messages;
 
 public class ErrorMessages {
+
+    private final static String INVALID_NAME = "";
+    private final static String INVALID_PHONE_NUMBER = "";
+    private final static String INVALID_ADDRESS = "";
+    private final static String INVALID_HOME_NUMBER = "";
+    private final static String INVALID_CITY_NAME = "";
+    private final static String INVALID_COUNTRY_NAME = "";
+
+    private final static String INVALID_COMMON_STRING = "";
 }
