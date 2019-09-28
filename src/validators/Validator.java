9cae45a6ea3c901b326364b738c3ef6d6cce21ca
zspace <zstefchev@gmail.com>
2019-09-28 21:00:12 +0300

    .

---
+++ src/validators/Validator.java
@@ -0,0 +1,33 @@
+package validators;
+
+public class Validator {
+
+    public static void checkName(){
+
+    }
+
+    public static void checkMobilePhoneNumber(){
+
+    }
+
+    public static void checkAddress(){
+
+    }
+
+    public static void checkHomePhoneNumber(){
+
+    }
+
+    public static void checkCityName(){
+
+    }
+
+    public static void checkCountryName(){
+
+    }
+
+    public static void checkCommonString(){
+
+    }
+
+}
